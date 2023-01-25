7368a5a65160bbddb4d4ae894d611cadce4b61c7
thiago <tmg3.mn@gmail.com>
2023-01-25 13:43:50 -0300

    Criação das routes do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/home/Home'
+import Thanks from './Pages/thanks/Thanks'
 import './App.scss'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='thanks' element={<Thanks />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
