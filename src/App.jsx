851120d3d48a403dce18201fd2e126d6339ec47e
thiago <tmg3.mn@gmail.com>
2023-01-26 11:04:54 -0300

    Classe '.appContainer' adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Thanks from './Pages/thanks/Thanks'
 import './App.scss'
 
 function App() {
+  
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='thanks' element={<Thanks />} />
-        </Routes>
-      </Router>
+      <div className="appContainer">
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='thanks' element={<Thanks />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
