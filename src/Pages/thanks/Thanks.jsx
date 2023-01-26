93ebd421c027d5de15a6d1ca22fca4532c099e9f
thiago <tmg3.mn@gmail.com>
2023-01-26 13:06:18 -0300

    JSX e estilos implementados em 'Thanks'

--- src/Pages/thanks/Thanks.jsx
+++ src/Pages/thanks/Thanks.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './thanks.scss'
 
-const Thanks = () => {
+const Thanks = ({ rating }) => {
   return (
-    <div>Thanks</div>
+    <div className='thanksContainer'>
+      <div className="imgContainerThanks">
+        <img src="assets/img/illustration-thank-you.svg" alt="" />
+      </div>
+      <div className="counterThanks">
+        <span className="counter">
+          You selected {rating} out of 5
+        </span>
+      </div>
+      <div className="infoThanks">
+        <h2>Thank you!</h2>
+        <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch</p>
+      </div>
+    </div>
   )
 }
 
